2fffb40a46641a4bb60a32a330d5ee6da660341a
Michael <skaman4459@gmail.com>
2020-07-16 17:01:15 -0400

    pagination and some styling
    Co-authored-by: Brittany Isaacson <brittanymae01@hotmail.com>
    Co-authored-by: David Hennig <davidjhennig@gmail.com>

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import '../styling/Cards.scss'
 
 const Cards = ({ search }) => {
+
     return(
 
-        <div>
+        <div className="results">
             {search.dropDown === 'repos' ? 
+
                 search.data?.items?.map(item => (
-                    <div className="repo-card" key={item.id}>
-                        <p><a href={item.owner.html_url}>{item.owner.login}</a></p>
-                        {/* link to user */}
-                        <p>{item.created_at}</p>
-                        <p><a href={item.html_url}>{item.name}</a></p>
-                        {/* link to project */}
-                        <p>{item.description}</p>
-                        <p>{item.language}</p>
-                        <p>{item.updated_at}</p>
+                    <div className="card-wrapper">
+                        <div className="repo-card" key={item.id}>
+                            <div className = 'img-div'>
+                                <img src={item.owner.avatar_url} />
+                            </div>
+                            <h3>Repository name: <a href={item.html_url}>{item.name}</a></h3>
+                            {/* link to project */}
+                            <p>Created by: <a href={item.owner.html_url}>{item.owner.login}</a></p>
+                            {/* link to user */}
+                            <p>Language: {item.language}</p>
+                            <p>Created on: {item.created_at}</p>
+                            <p>Last updated on: {item.updated_at}</p>
+                            <p>Description: {item.description}</p>
+                        </div>
                     </div>
-                )) :
+                    )) :
 
                 search.data?.items?.map(item => (
                     <div className="repo-card" key={item.id}>
@@ -24,7 +32,9 @@ const Cards = ({ search }) => {
                         {/* link to user */}
                     </div>
                 ))
-        }
+            }
+
+
         </div>
     )}
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -12,6 +12,18 @@ const Search = () => {
         data: {}
     })
 
+    const perPage = 10;
+    let numPages = Math.ceil(search.data?.total_count / perPage);
+    
+    // for pagination
+    const pageNumbers = []
+    for(let i = 1; i <= numPages; i++) {
+        if(numPages > 100) {
+            numPages = 100;
+        }
+        pageNumbers.push(i)
+    }
+
     const handleChange = e => {
         setSearch({
             ...search,
@@ -26,21 +38,22 @@ const Search = () => {
             data: {}
         })
     }
-    const handleSubmit = e => {
+    const  handleSubmit = e => {
         e.preventDefault()
         search.dropDown === 'repos' ? 
             // check whether dropdown is repos or users, 
             // and then make a call to their respective URL's
 
-            axios.get(`https://api.github.com/search/repositories?q=${search.searchTerm}&page=${search.page}&per_page=10`)
+            axios.get(`https://api.github.com/search/repositories?q=${search.searchTerm}&page=${search.page}&per_page=${perPage}`)
             .then(res => {
                 setSearch({...search, data: res.data});
+                console.log(res)
             })
             .catch(error => {
                 console.log(error)
             }) :
             
-            axios.get(`https://api.github.com/search/users?q=${search.searchTerm}&page=${search.page}&per_page=10`)
+            axios.get(`https://api.github.com/search/users?q=${search.searchTerm}&page=${search.page}&per_page=${perPage}`)
             .then(res => {
                 setSearch({...search, data: res.data});
             })
@@ -67,7 +80,19 @@ const Search = () => {
             </form>
 
             <Cards search={search} />
-            
+            <p>
+                {pageNumbers.map(number => (
+                   <span onClick={async () => {
+                       setSearch(search.page = number)
+                       const res = await axios.get(`https://api.github.com/search/repositories?q=${search.searchTerm}&page=${search.page}&per_page=${perPage}`)
+                       setSearch({...search, data: res.data})
+                    }
+                   }>
+                       {number}
+                    </span> 
+                ))}
+
+            </p>
         </div>
     )}
 
